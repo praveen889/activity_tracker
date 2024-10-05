41168ea57c514c772c710dd72be3dc6c72fbabf0
Praveen <60999279+praveen889@users.noreply.github.com>
2024-10-05 21:28:37 +0530

    Update screenshot_manager.py

--- tracking/screenshot_manager.py
+++ tracking/screenshot_manager.py
@@ -2,36 +2,43 @@ import time
 import threading
 import pyautogui
 import os
-from PIL import ImageFilter
 
 class ScreenshotManager:
-    def __init__(self, interval=300, blur=False, output_dir="static/screenshots"):
+    def __init__(self, user_id, log_callback, upload_callback=None, interval=60):
+        self.user_id = user_id
+        self.log_callback = log_callback
+        self.upload_callback = upload_callback
         self.interval = interval
-        self.blur = blur
-        self.output_dir = output_dir
         self.active = False
-        if not os.path.exists(self.output_dir):
-            os.makedirs(self.output_dir)
+        self.capture_thread = None
 
     def capture_screenshot(self):
         screenshot = pyautogui.screenshot()
-        if self.blur:
-            screenshot = screenshot.filter(ImageFilter.GaussianBlur(15))
-        file_path = os.path.join(self.output_dir, f'screenshot_{int(time.time())}.png')
+        file_path = os.path.join('static/screenshots', f'screenshot_{int(time.time())}.png')
         screenshot.save(file_path)
+        self.log_callback(screenshot_path=file_path) 
         print(f"Screenshot saved: {file_path}")
 
     def start_capturing(self):
-        self.active = True
-        print("Starting screenshot capturing...")
+        if not self.active:
+            self.active = True
+            print("Starting screenshot capturing...")
+            self.capture_thread = threading.Thread(target=self.capture_loop)
+            self.capture_thread.start()
+
+    def capture_loop(self):
         while self.active:
+            print("Capturing screenshot...") 
             self.capture_screenshot()
             time.sleep(self.interval)
+        print("Capture loop has exited.") 
 
     def stop_capturing(self):
+        print("Attempting to stop screenshot capturing...")
         if self.active:
-            print("Stopping screenshot capturing...")
             self.active = False 
+            if self.capture_thread is not None:
+                self.capture_thread.join() 
             print("Screenshot capturing stopped.")
         else:
             print("Screenshot capturing was not active.")
